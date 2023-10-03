d6d3a118c138952c4070c5681ded9f674668155f
projeta618 <projeta618@gmail.com>
2023-10-03 09:08:52 -0300

    Init the new link manager

--- models.py
+++ models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from kernel.models.base_metadata_model import BaseMetadataModel
 
-# Create your models here.
+class Link(BaseMetadataModel):
+    """
+        @description: 
+    """
+    host = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    queryParams = models.CharField(max_length=255)
+    hash = models.CharField(max_length=255)
+    full_url = models.CharField(max_length=255)
+
+    def __str__(self):
+        """
+            @description:
+        """
+        return self.host + self.path + self.queryParams + self.hash
+    
+class LinkOpened(BaseMetadataModel):
+    """
+        @description: 
+    """
+    profile = models.ForeignKey(
+        'profiles.Profile', 
+        related_name='link_opened_profile',
+        on_delete=models.CASCADE,
+    )
+
+    link = models.ForeignKey(
+        'Link', 
+        related_name='link_opened_link',
+        on_delete=models.CASCADE,
+    )
+
+    counter = models.IntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        """
+            @description:
+        """
+        return self.profile + self.link
